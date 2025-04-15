d1004cebf7c0bc44ca45d890545d4c886f011a4f
Friedinger <info@friedinger.org>
2025-04-15 19:38:36 +0200

    ♻️ Improve js

--- infoscreen.js
+++ infoscreen.js
@@ -1,121 +1,152 @@
-window.onload = () => {
-	fetch("config.json?t=" + new Date().getTime())
-		.then((response) => response.json())
-		.then((config) => {
-			departures(config);
-			news(config);
-			setTimeout(() => {
-				location.reload(true);
-			}, (config.reloadInterval ?? 60) * 1000);
-		});
-};
+window.onload = async () => {
+	const defaultConfig = {
+		reloadInterval: 60,
+		newsInterval: 20,
+		departureInterval: 20,
+	};
 
-function news(config) {
-	const newsDisplay = document.querySelector("#news");
-	const newsBackground = newsDisplay.parentElement;
-	const news = config.news;
-	if (config.news.length == 0) return;
-	let currentNews = 0;
-	newsDisplay.setAttribute("src", "news/" + news[currentNews]);
-	newsBackground.style.setProperty(
-		"--url",
-		"url('news/" + news[currentNews] + "')"
-	);
-	if (config.news.length == 1) return;
-	setInterval(() => {
-		currentNews++;
-		if (currentNews >= news.length) currentNews = 0;
-		newsDisplay.setAttribute("src", "news/" + news[currentNews]);
-		newsBackground.style.setProperty(
-			"--url",
-			"url('news/" + news[currentNews] + "')"
-		);
-	}, (config.newsInterval ?? 20) * 1000);
-}
+	const config = await loadConfig();
+	departures();
+	news();
+	setTimeout(() => {
+		location.reload(true);
+	}, getConfigValue("reloadInterval") * 1000);
 
-function departures(config) {
-	loadDepartures(config.departureUrl);
-	setInterval(() => {
-		loadDepartures(config.departureUrl);
-	}, (config.departureInterval ?? 20) * 1000);
-}
-
-function loadDepartures(url) {
-	fetch(url + "&t=" + new Date().getTime())
-		.then((response) => response.json())
-		.then((data) => {
-			let departure = document.querySelector(".departure table");
-			departure.querySelectorAll(".tr").forEach((element) => {
-				element.remove();
-			});
-			data.forEach((element) => {
-				let timeReal = element.realtimeDepartureTime;
-				let timeIn =
-					element.realtimeDepartureTime - new Date().getTime();
-				let timeDelay =
-					element.realtimeDepartureTime -
-					element.plannedDepartureTime;
-
-				let row = document.createElement("tr");
-				row.classList.add("tr");
-				row.innerHTML = `
-					<td class="line">
-						<span class="type ${element.transportType}"></span>
-						<span class="label">${textOutput(element.label)}</span>
-					</td>
-					<td class="destination">
-						${textOutput(element.destination)}
-					</td>
-					<td class="platform">
-						${textOutput(element.platform, element.stopPositionNumber)}
-					</td>
-					<td class="time real">
-						${timeOutput(timeReal)}
-					</td>
-					<td class="time in">
-						${timeOutput(timeIn, "offset")}
-					</td>
-					${delayOutput(timeDelay)}
-				`;
-				departure.appendChild(row);
-			});
-		});
-}
-
-function textOutput() {
-	for (const element of arguments) {
-		if (element != null) return element;
+	async function loadConfig() {
+		try {
+			const response = await fetch(
+				"config.json?t=" + new Date().getTime()
+			);
+			if (!response.ok)
+				throw new Error("Fehler beim Laden der Konfigurationsdatei");
+			return await response.json();
+		} catch (error) {
+			console.error("Fehler beim Laden der Konfigurationsdatei: ", error);
+		}
 	}
-	return "";
-}
 
-function timeOutput(time, format = "clock") {
-	let output;
-	switch (format) {
-		case "clock":
+	function news() {
+		const newsDisplay = document.querySelector("#news");
+		const newsBackground = newsDisplay.parentElement;
+		const news = config.news;
+		if (config.news.length == 0) return;
+
+		function setNews(news) {
+			newsDisplay.src = "news/" + news;
+			newsBackground.style.setProperty("--url", `url('news/${news}')`);
+		}
+
+		let currentNews = 0;
+		setNews(news[currentNews]);
+
+		if (config.news.length == 1) return;
+		setInterval(() => {
+			currentNews = (currentNews + 1) % news.length;
+			setNews(news[currentNews]);
+		}, getConfigValue("newsInterval") * 1000);
+	}
+
+	function departures(config) {
+		const departureTable = document.querySelector(".departure table");
+		updateDepartures(config.departureUrl);
+		setInterval(() => {
+			updateDepartures(config.departureUrl);
+		}, getConfigValue("departureInterval") * 1000);
+
+		async function updateDepartures(url) {
+			try {
+				const response = await fetch(
+					url + "&t=" + new Date().getTime()
+				);
+				if (!response.ok)
+					throw new Error(
+						`Fehler beim Laden der Abfahrtsdaten (HTTP-Status: ${response.status})`
+					);
+				const departures = await response.json();
+				departureTable
+					.querySelectorAll("tr:not(.th)")
+					.forEach((row) => row.remove());
+				departures.forEach((departure) => {
+					departureTable.appendChild(createRow(departure));
+				});
+			} catch (error) {
+				console.error(
+					"Fehler beim Aktualisieren der Abfahrtsdaten: ",
+					error
+				);
+			}
+		}
+
+		function createRow(departure) {
+			const timeReal = departure.realtimeDepartureTime;
+			const timeIn =
+				departure.realtimeDepartureTime - new Date().getTime();
+			const timeDelay =
+				departure.realtimeDepartureTime -
+				departure.plannedDepartureTime;
+
+			const row = document.createElement("tr");
+			row.innerHTML = `
+				<td class="line">
+					<span class="type ${departure.transportType}"></span>
+					<span class="label">${textOutput(departure.label)}</span>
+				</td>
+				<td class="destination">
+					${textOutput(departure.destination)}
+				</td>
+				<td class="platform">
+					${textOutput(departure.platform, departure.stopPositionNumber)}
+				</td>
+				<td class="time real">
+					${timeClockOutput(timeReal)}
+				</td>
+				<td class="time in">
+					${timeOffsetOutput(timeIn)}
+				</td>
+				${delayOutput(timeDelay)}
+			`;
+			return row;
+		}
+
+		function textOutput(...options) {
+			for (const element of options) {
+				if (element != null) return element;
+			}
+			return "";
+		}
+
+		function timeClockOutput(time) {
 			return new Date(time).toLocaleTimeString("de-DE", {
 				hour: "numeric",
 				minute: "numeric",
 			});
-		case "offset":
-			output = Math.round(new Date(time).getTime() / 1000 / 60);
+		}
+
+		function timeOffsetOutput(time) {
+			const output = Math.round(new Date(time).getTime() / 1000 / 60);
 			if (output <= 0) return "Jetzt";
 			return output + " min";
-		case "delay":
-			output = Math.round(new Date(time).getTime() / 1000 / 60);
+		}
+
+		function timeDelayOutput(time) {
+			const output = Math.round(new Date(time).getTime() / 1000 / 60);
 			if (output > 0) return "+ " + output + " min";
 			if (output < 0) return "- " + Math.abs(output) + " min";
-			if (output == 0) return "pünktlich";
-	}
-}
+			return "pünktlich";
+		}
 
-function delayOutput(timeDelay) {
-	let time = timeOutput(timeDelay, "delay");
-	let color = "intime";
-	if (time.startsWith("+")) color = "delayed";
-	return `
-		<td class="time delay ${color}">
-			${time}
-		</td>
-	`;
-}
+		function delayOutput(timeDelay) {
+			const time = timeDelayOutput(timeDelay);
+			const color = time.startsWith("+") ? "delayed" : "intime";
+			return `
+				<td class="time delay ${color}">
+					${time}
+				</td>
+			`;
+		}
+	}
+
+	function getConfigValue(key) {
+		return config[key] ?? defaultConfig[key];
+	}
+};
